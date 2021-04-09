f7fdf915824d9d4f21a55714fe930db1753546db
MesteRobot <hassan.doodiyan@gmail.com>
2021-04-09 13:53:37 +0430

    create detail view for blog app and link the blogs to all blog page to see the details"

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 import math
 
 
-def blogs(request):
+def blog(request):
     page_number = 1
     blogs_per_page = 5
     all_pages = math.ceil(Blog.objects.all().count()/5)
@@ -12,6 +12,9 @@ def blogs(request):
     if request.GET.get("prev") and page_number > 1:
         page_number -= 1
     blogs = Blog.objects.order_by('-date_created')[(page_number - 1) * blogs_per_page:page_number * blogs_per_page]
-    return render(request, 'blog/blogs.html', {'blogs': blogs, 'page_number': page_number, 'all_pages': all_pages})
+    return render(request, 'blog/blog.html', {'blogs': blogs, 'page_number': page_number, 'all_pages': all_pages})
 
 
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, "blog/detail.html", {'blog': blog})
