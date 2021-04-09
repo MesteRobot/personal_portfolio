91c02727aa00dd08982ebcb6f6a7845429022423
MesteRobot <hassan.doodiyan@gmail.com>
2021-04-09 16:41:17 +0430

    som details in views

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,15 @@ import math
 
 
 def blog(request):
-    page_number = 1
-    blogs_per_page = 5
-    all_pages = math.ceil(Blog.objects.all().count()/5)
-    if request.GET.get("next") and page_number <= all_pages:
-        page_number += 1
-    if request.GET.get("prev") and page_number > 1:
-        page_number -= 1
-    blogs = Blog.objects.order_by('-date_created')[(page_number - 1) * blogs_per_page:page_number * blogs_per_page]
-    return render(request, 'blog/blog.html', {'blogs': blogs, 'page_number': page_number, 'all_pages': all_pages})
+    # page_number = 1
+    # blogs_per_page = 5
+    # all_pages = math.ceil(Blog.objects.all().count()/5)
+    # if request.GET.get("next") and page_number <= all_pages:
+    #     page_number += 1
+    # if request.GET.get("prev") and page_number > 1:
+    #     page_number -= 1
+    blogs = Blog.objects.order_by('-date_created')
+    return render(request, 'blog/blog.html', {'blogs': blogs})
 
 
 def detail(request, blog_id):
